165f6cfbc8c8cc59d068a36003d8c1d49026bb82
koushik-ai-distsys <gridfinsdeploy@gmail.com>
2020-06-16 18:20:05 +0530

    dataset.py modified to add any video to folder

--- opencv-ad-rendering-v1/dataset.py
+++ opencv-ad-rendering-v1/dataset.py
@@ -5,11 +5,14 @@ import os
 import cv2
 
 # build the VideoCapture object
-video_path = "./data/video/demo_video.mp4"
-video = cv2.VideoCapture(video_path)
+video_path = "./data/video/"
+video_name = os.listdir(video_path)[0]
+
+video = cv2.VideoCapture(video_path+video_name)
 
 # build a list of advertisements (images)
 images_path = "./data/images/"
 image_list = os.listdir(images_path)
 images = [cv2.imread(images_path+img) for img in image_list]
+
 advertisements = [cv2.resize(image, (400, 400)) for image in images]
